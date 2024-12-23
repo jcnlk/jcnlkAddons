8e1f4474269f4ea1f8dbd8517cf81704bedded90
jcnlk <itsjxnik@gmail.com>
2024-12-23 16:51:29 +0100

    Typo

--- data/DungeonLoot.js
+++ data/DungeonLoot.js
@@ -78,7 +78,7 @@ export const greenItems = [
     "SADAN_BROOCH",
     "NECROMANCER_LORD_LEGGINGS",
     "NECROMANCER_SWORD",
-    "GOLDOR_FISH",
+    "GOLDOR_THE_FISH",
     "MAXOR_THE_FISH",
     "STORM_THE_FISH",
     "ADAPTIVE_BELT"
